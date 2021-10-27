ce156d2a15853e21f88321a5f8072aed61dac8a6
Mirgahney Mohamed <mmerghaney@aimsammi.org>
2021-10-27 23:17:47 +0100

    add next lab notebook

--- labs/utils.py
+++ labs/utils.py
@@ -1,5 +1,14 @@
+import numpy as np
+import copy
+import cv2
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
+import random
+from PIL import Image
+from typing import Union
 from sklearn.feature_extraction.image import check_array, _extract_patches, \
 _compute_n_patches, check_random_state
+
 
 def extract_patches_2d(image, patch_size, *, max_patches=None,
                        random_state=None):
@@ -72,3 +81,52 @@ def extract_patches_2d(image, patch_size, *, max_patches=None,
         return patches.reshape((n_patches, p_h, p_w))
     else:
         return patches
+
+
+def resize(img, scale: Union[float, int]) -> np.ndarray:
+    """ Resize an image maintaining its proportions
+    Args:
+        fp (str): Path argument to image file
+        scale (Union[float, int]): Percent as whole number of original image. eg. 53
+    Returns:
+        image (np.ndarray): Scaled image
+    """
+    _scale = lambda dim, s: int(dim * s / 100)
+    height, width, channels = img.shape
+    new_width: int = _scale(width, scale)
+    new_height: int = _scale(height, scale)
+    new_dim: tuple = (new_width, new_height)
+    return cv2.resize(src=img, dsize=new_dim, interpolation=cv2.INTER_LINEAR)
+
+def colourize(img):
+    height, width = img.shape
+
+    colors = []
+    colors.append([])
+    colors.append([])
+    color = 1
+    # Displaying distinct components with distinct colors
+    coloured_img = Image.new("RGB", (width, height))
+    coloured_data = coloured_img.load()
+
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if img[i][j] > 0:
+                if img[i][j] not in colors[0]:
+                    colors[0].append(img[i][j])
+                    colors[1].append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+                ind = colors[0].index(img[i][j])
+                coloured_data[j, i] = colors[1][ind]
+
+    return coloured_img
+
+
+def binarize(img_array, threshold=130):
+    for i in range(len(img_array)):
+        for j in range(len(img_array[0])):
+            if img_array[i][j] > threshold:
+                img_array[i][j] = 0
+            else:
+                img_array[i][j] = 1
+    return img_array
