14fa0c979c46f6f83ccfd287f09977e7bb1dc76c
JingwenWang95 <jingwen.wang.17@ucl.ac.uk>
2023-10-26 14:42:31 +0100

    added lab week4

--- labs/utils.py
+++ labs/utils.py
@@ -158,16 +158,8 @@ def order_points(pts):
     # return the ordered coordinates
     return rect
 
-def four_point_transform(image, pts, dst=None):
-    '''
-    
-    code adapted from
-    https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
-    
-    '''
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
+
+def get_max_hw(rect):
     (tl, tr, br, bl) = rect
     # compute the width of the new image, which will be the
     # maximum distance between bottom-right and bottom-left
@@ -181,17 +173,28 @@ def four_point_transform(image, pts, dst=None):
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
+    return maxHeight, maxWidth
+
+
+def four_point_transform(image, pts, dst):
+    '''
+    
+    code adapted from
+    https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+    
+    '''
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+
     # now that we have the dimensions of the new image, construct
     # the set of destination points to obtain a "birds eye view",
     # (i.e. top-down view) of the image, again specifying points
     # in the top-left, top-right, bottom-right, and bottom-left
+
     # order
-    if dst is None:
-        dst = np.array([
-            [0, 0],
-            [maxWidth - 1, 0],
-            [maxWidth - 1, maxHeight - 1],
-            [0, maxHeight - 1]], dtype = "float32")
+    dst = order_points(dst)
+    maxHeight, maxWidth = get_max_hw(dst)
 
     # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
